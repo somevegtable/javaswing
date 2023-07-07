e1acc3a2bdbe5f57cef641112cb609ea0d75e654
userName <576592591@qq.com>
2023-07-07 15:48:21 +0800

    管理员模块完成

--- src/admin/HealthManageShow.java
+++ src/admin/HealthManageShow.java
@@ -1,65 +1,89 @@
 package admin;
 
+import admin.buttonclick.*;
 import dao.DAO;
+import entity.Baoan;
+import entity.Old;
+import hugong.UpdateHealthShow;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HealthManageShow {
     public static void createShow() throws SQLException {
         JFrame frame = new JFrame();
         frame.setSize(600, 600);
         frame.setLocationRelativeTo(null);
-        frame.setTitle("床位信息");
+        frame.setTitle("健康管理");
 
         JPanel panel = new JPanel();
         panel.setLayout(null);
 
-        //查询床位信息
+        //查询old信息
         DAO dao = new DAO();
-        ResultSet resultSet = dao.query("select * from t_bed");
+        ResultSet resultSet = dao.query("select * from t_old");
 
         //ResultSet转换为List
-        /*List<Bed> bedList = new ArrayList<>();
+        List<Old> oldList = new ArrayList<>();
         while(resultSet.next()){
-            Bed bed = new Bed();
-            bed.setId(resultSet.getInt(1));
-            bed.setCode(resultSet.getString(2));
-            bed.setStatus(resultSet.getString(3));
-            bed.setHolder(resultSet.getString(4));
-            bedList.add(bed);
-        }*/
+            Old old = new Old();
+            old.setId(resultSet.getInt(1));
+            old.setName(resultSet.getString(2));
+            old.setGender(resultSet.getString(3));
+            old.setPlace(resultSet.getString(4));
+            old.setStatus(resultSet.getString(5));
+            old.setPhone(resultSet.getString(6));
+            oldList.add(old);
+        }
+
+        JButton updateUser = new JButton("修改健康信息");
+        updateUser.setBounds(250, 50, 120, 25);
+        updateUser.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    AdminUpdateHealthShow.createShow();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+                frame.setVisible(false);
+            }
+        });
+
+        panel.add(updateUser);
+        frame.add(panel);
 
         //创建JTable
-        /*JTable table;
-        String [] index = {"id","床位码","状态","占用者"};
-        Object [][] data = new Object[bedList.size()][index.length];
+        JTable table;
+        String [] index = {"id","姓名","性别","床位", "电话", "健康状态"};
+        Object [][] data = new Object[oldList.size()][index.length];
         //3,向data中添加数据
-        for (int i = 0; i < bedList.size(); i++) {
-            Bed bed = bedList.get(i);
-            data[i][0] = bed.getId();
-            data[i][1] = bed.getCode();
-            data[i][2] = bed.getStatus();
-            data[i][3] = bed.getHolder();
-        }*/
+        for (int i = 0; i < oldList.size(); i++) {
+            Old user = oldList.get(i);
+            data[i][0] = user.getId();
+            data[i][1] = user.getName();
+            data[i][2] = user.getGender();
+            data[i][3] = user.getPlace();
+            data[i][4] = user.getPhone();
+            data[i][5] = user.getStatus();
+        }
 
         //4,创建一个默认的表格模型
-        /*DefaultTableModel defaultModel = new DefaultTableModel(data, index);
+        DefaultTableModel defaultModel = new DefaultTableModel(data, index);
         table=new JTable(defaultModel);
-        table.setBackground(Color.WHITE);
-        table.setPreferredScrollableViewportSize(new Dimension(100, 80));//JTable的高度和宽度按照设定
-        table.setFillsViewportHeight(true);
+        table.setBackground(Color.white);
+        table.setBounds(0, 100, 600, 400);
 
         //5，给表格设置滚动条
-        JScrollPane jScrollPane = new JScrollPane();
-        jScrollPane.setViewportView(table);*/
-
-        /*frame.add(panel, BorderLayout.NORTH);
-        frame.setVisible(true);
-        frame.add(jScrollPane,BorderLayout.CENTER);
-
+        panel.add(table);
+        frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setVisible(true);*/
+        frame.setVisible(true);
     }
 }
---
+++ src/admin/buttonclick/AdminUpdateHealthShow.java
@@ -0,0 +1,100 @@
+package admin.buttonclick;
+
+import admin.HealthManageShow;
+import admin.SalaryManageShow;
+import com.mysql.cj.util.StringUtils;
+import dao.DAO;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdminUpdateHealthShow {
+    public static void createShow() throws SQLException {
+        JFrame frame = new JFrame();
+        frame.setSize(600, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setTitle("健康信息登记");
+
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
+        //创建老人id标签
+        JLabel idLabel = new JLabel("老人编号：");
+        idLabel.setFont(new Font("微软雅黑", 0, 13));
+        idLabel.setBounds(180, 100, 80, 25);
+        panel.add(idLabel);
+
+        //创建老人id输入文字域
+        JTextField idText = new JTextField(20);
+        idText.setBounds(250, 100, 165, 25);
+        panel.add(idText);
+
+        //创建老人健康状态标签
+        JLabel statusLabel = new JLabel("健康状态：");
+        statusLabel.setFont(new Font("微软雅黑", 0, 13));
+        statusLabel.setBounds(180, 150, 80, 25);
+        panel.add(statusLabel);
+
+        //创建老人健康状态输入文字域
+        JTextField statusText = new JTextField(20);
+        statusText.setBounds(250, 150, 165, 25);
+        panel.add(statusText);
+
+        JButton confirm = new JButton("登记");
+        confirm.setBounds(250, 250, 80, 25);
+        panel.add(confirm);
+        confirm.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (StringUtils.isNullOrEmpty(idText.getText())){
+                    JOptionPane.showMessageDialog(null, "id不能为空！", "失败", 0);
+                }
+                String id = idText.getText();
+
+                DAO dao = new DAO();
+                ResultSet resultSet = dao.query("select * from t_old where id = "+id+"");
+                try {
+                    if (resultSet.next() && !StringUtils.isNullOrEmpty(statusText.getText())){
+                        Integer rows = dao.update("update t_old set status = '" + statusText.getText() + "' where id = " + id + "");
+                        System.out.println("影响的行数：" + rows);
+                        if (rows > 0){
+                            JOptionPane.showMessageDialog(null, "登记成功！", "成功", 1);
+                        }else{
+                            JOptionPane.showMessageDialog(null, "登记失败！", "失败", 0);
+                            frame.setVisible(false);
+                        }
+                    }else if (StringUtils.isNullOrEmpty(idText.getText()) || StringUtils.isNullOrEmpty(statusText.getText())){
+                        JOptionPane.showMessageDialog(null, "参数不能为空！", "失败", 0);
+                        frame.setVisible(false);
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, "参数信息不合法！", "失败", 0);
+                    }
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                try {
+                    HealthManageShow.createShow();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+        });
+
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
